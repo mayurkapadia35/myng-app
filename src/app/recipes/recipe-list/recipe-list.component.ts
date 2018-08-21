d5e57b697e7cd7236fe4fbd400f07c66603e4e3b
mayurkapadia35 <lanetteam.mayur35@gmail.com>
2018-08-21 16:05:45 +0530

    Make a skeleton of the Recipe-book.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'A simple test description', 'https://i2.wp.com/media.hungryforever.com/' +
+      'wp-content/uploads/2016/09/14131701/best-ice-cream-in-mumbai.jpg?ssl=1?w=356&strip=all&quality=80')
+  ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
