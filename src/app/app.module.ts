2238e9ff04ff94b50fbc95f21a2c4572b02b4961
mayurkapadia35 <lanetteam.mayur35@gmail.com>
2018-08-22 16:03:15 +0530

    Add a service.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewAccountComponent,
+    AccountComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
