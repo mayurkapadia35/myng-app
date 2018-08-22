0c3c544297cdaf3ccaea582db02c525516d49e51
mayurkapadia35 <lanetteam.mayur35@gmail.com>
2018-08-22 18:02:58 +0530

    add services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,11 @@
+import {Recipe} from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'A simple test description', 'https://i2.wp.com/media.hungryforever.com/' +
+      'wp-content/uploads/2016/09/14131701/best-ice-cream-in-mumbai.jpg?ssl=1?w=356&strip=all&quality=80')
+  ];
+  getRecipes () {
+    return this.recipes;
+  }
+}
