408cda4f97416c2d8e9ac04e0f6196a4c9637e88
mayurkapadia35 <lanetteam.mayur35@gmail.com>
2018-08-23 12:33:11 +0530

    Show a ingredients on detail page & add them into the shopping-list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -17,4 +17,12 @@ export class ShoppingListService {
     this.ingredientChanged.emit(this.ingredients.slice());
   }
 
+  addIngredients(ingredients: Ingredient[]) {
+    // for (const ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // }
+    this.ingredients.push(...ingredients);
+    this.ingredientChanged.emit(this.ingredients.slice());
+  }
+
 }
